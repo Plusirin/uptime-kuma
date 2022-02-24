a96f2669d20803b4e52a710e35700adb6c67ee74
张云鹏 <370397727@qq.com>
2022-02-24 15:25:24 +0800

    Update build.sh dockerfile-owner

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
+docker login --username=yunpeng@burgeon-zhizao registry.cn-shanghai.aliyuncs.com
 buildx create --use --name uptime-kuma
 docker buildx inspect uptime-kuma --bootstrap
-docker buildx build -t registry.cn-shanghai.aliyuncs.com/r3-dev/uptime-kuma --platform=linux/arm,linux/arm64,linux/amd64 -f dockerfile-owner . --push
+docker buildx build -f dockerfile-owner --platform=linux/arm64,linux/amd64 -t registry.cn-shanghai.aliyuncs.com/r3-dev/uptime-kuma:v20200224 . --push
 docker buildx build -t ccr.ccs.tencentyun.com/plusirin/uptime-kuma --platform=linux/arm64,linux/amd64 -f dockerfile-owner . --push
